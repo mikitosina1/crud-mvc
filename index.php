d01f69d0bf54c06c6888f1c49fcb22253691cb51
mikitosina1 <mikitosina@mail.ru>
2020-06-08 16:39:06 +0300

    second commit добавлено подключение к бд, настроен приятный интерфейс и начата разработка crud

--- index.php
+++ index.php
@@ -13,7 +13,27 @@ error_reporting(E_ALL);
 
 <body>
 	<div class="container-fluid">
-			<?php include './lib/nav.php';?>
+
+		<?php
+		include_once './lib/nav.php';	
+		include './lib/db.php';
+		include './lib/create.php';
+		include './lib/read.php';
+		include './lib/update.php';
+		include './lib/delete.php';
+		?>
+		
+		<input type="text" class="form-control mt-2" placeholder="1 поле" aria-label="Имя пользователя" aria-describedby="basic-addon1">
+		<input type="text" class="form-control mt-2" placeholder="2 поле" aria-label="Имя пользователя" aria-describedby="basic-addon1">
+		<input type="text" class="form-control mt-2" placeholder="3 поле" aria-label="Имя пользователя" aria-describedby="basic-addon1">
+		<button type="button" class="btn btn-success mt-2 ml-1 mr-2">Создать</button>
+		<button type="button" class="btn btn-info mt-2 ml-1 mr-1">Обновить</button>
+		<button type="button" class="btn btn-danger mt-2 ml-2 mr-1">Удалить</button>
+
+
+
+
+
 		<footer>
 			<?php include './lib/footer.php';?>
 		</footer>
---
+++ lib/db.php
@@ -0,0 +1,24 @@
+<?php
+$host = 'localhost';
+$db = 'cars';
+$user = 'root';
+$password = '';
+$db_connect = mysqli_connect($host, $user, $password, $db);
+if ($db_connect == false){
+    print("<div class='alert alert-danger alert-dismissible fade show mb-2 mt-2' role='alert'>
+			Невозможно подключиться к БД
+			<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+			<span aria-hidden='true'>&times;</span>
+		</div>" . mysqli_connect_error());
+}
+else {
+    print(
+		"<div class='alert alert-success alert-dismissible fade show bm-2 mt-2' role='alert'>
+			подключено к БД
+			<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+			<span aria-hidden='true'>&times;</span>
+		</div>"
+	);
+}
+mysqli_query($db_connect, "SET NAMES utf8");
+?>
